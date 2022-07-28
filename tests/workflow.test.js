373fad549a34c60ba330b69afeb063bb35c7ce63
tjduigna <thomas.j.duignan@gmail.com>
2022-07-28 18:45:45 -0400

    feat: published code and friends

--- tests/workflow.test.js
+++ tests/workflow.test.js
@@ -1,11 +1,6 @@
-import { Template } from "@exabyte-io/ade.js";
 import { expect } from "chai";
 
-import { ContextProviderRegistry } from "../src/context/registry";
 import { createWorkflows } from "../src/workflows";
-
-// patch to make all context providers available
-Template.providerRegistry = ContextProviderRegistry;
 
 describe("workflows", () => {
     it("can all be created", () => {
