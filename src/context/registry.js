28e48ed1a5ef19d6a43ffa1a9d95728b7ce72789
Alexander Zech <alexzech@gmx.de>
2022-08-30 18:29:40 -0700

    refactor: add function to create context provider registry

--- src/context/registry.js
+++ src/context/registry.js
@@ -3,11 +3,19 @@ import _ from "underscore";
 
 import * as allContextProviders from "./providers";
 
-_.map(allContextProviders, (instance, name) =>
-    clsInstance.addProvider({
-        instance,
-        name,
-    }),
-);
+/** Create registry of context providers
+ *
+ * @summary: Adds context providers to registry and patches them with a static
+ * class variable if classes are provided with `classesToPatch`.
+ */
+export const createRegistry = (classesToPatch = []) => {
+    return _.map(allContextProviders, (instance, name) => {
+        classesToPatch.forEach((cls) => (instance[cls.name] = cls));
+        return clsInstance.addProvider({
+            instance,
+            name,
+        });
+    });
+};
 
-export const ContextProviderRegistry = clsInstance;
+export const ContextProviderRegistry = createRegistry();
