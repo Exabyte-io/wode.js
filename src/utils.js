50a49722645b1dad4f638464dc64ad579c781942
Sean Warren <seankwarren722@gmail.com>
2023-06-13 20:07:14 -0700

    chore: revert

--- src/utils.js
+++ src/utils.js
@@ -22,7 +22,8 @@ export function setUnitsHead(units) {
 export function setNextLinks(units) {
     const flowchartIds = units.map((u) => u.flowchartId);
     for (let i = 0; i < units.length - 1; i++) {
-        if (!units[i].next || units[i].next !== units[i + 1].flowchartId) {
+        if (!units[i].next) {
+            console.log("wode is updated!");
             // newly added units don't have next set yet => set it
             units[i].next = units[i + 1].flowchartId;
             if (i > 0) units[i - 1].next = units[i].flowchartId;
