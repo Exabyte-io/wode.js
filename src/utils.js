fbd334245cd95b51ec2c236381a42cfa2ccbd45f
Sean Warren <seankwarren722@gmail.com>
2023-06-13 18:08:46 -0700

    fix: previous links correctly updated

--- src/utils.js
+++ src/utils.js
@@ -22,7 +22,7 @@ export function setUnitsHead(units) {
 export function setNextLinks(units) {
     const flowchartIds = units.map((u) => u.flowchartId);
     for (let i = 0; i < units.length - 1; i++) {
-        if (!units[i].next) {
+        if (!units[i].next || units[i].next !== units[i + 1].flowchartId) {
             // newly added units don't have next set yet => set it
             units[i].next = units[i + 1].flowchartId;
             if (i > 0) units[i - 1].next = units[i].flowchartId;
