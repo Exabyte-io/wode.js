b29f510eb17236ec0c6df139779e86428594204f
Kostiantyn Dvornik <dvornik.konstantin@gmail.com>
2024-02-21 22:19:37 +0200

    chore: rmove try/catch in tests

--- tests/unit.test.js
+++ tests/unit.test.js
@@ -4,19 +4,13 @@ import { createWorkflows } from "../src/workflows";
 
 describe("units", () => {
     it("can be cloned with new flowchartId", () => {
-        try {
-            const workflows = createWorkflows({});
-            const exampleWorkflow = workflows[0];
-            const exampleSubworkflow = exampleWorkflow.subworkflows[0];
-            const exampleUnit = exampleSubworkflow.units[0];
-            const exampleUnitClone = exampleUnit.clone();
-            // eslint-disable-next-line no-unused-expressions
-            expect(exampleUnitClone).to.exist;
-            expect(exampleUnit.flowchartId).to.not.equal(exampleUnitClone.flowchartId);
-        } catch (err) {
-            console.log({
-                error: err.error,
-            });
-        }
+        const workflows = createWorkflows({});
+        const exampleWorkflow = workflows[0];
+        const exampleSubworkflow = exampleWorkflow.subworkflows[0];
+        const exampleUnit = exampleSubworkflow.units[0];
+        const exampleUnitClone = exampleUnit.clone();
+        // eslint-disable-next-line no-unused-expressions
+        expect(exampleUnitClone).to.exist;
+        expect(exampleUnit.flowchartId).to.not.equal(exampleUnitClone.flowchartId);
     });
 });
