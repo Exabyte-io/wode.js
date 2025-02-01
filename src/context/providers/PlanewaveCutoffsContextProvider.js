e18fefd7b7d8a86eba59b9694a5c26cc5df4eb11
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2025-02-01 21:10:01 +0800

    SOF-7449: set wfc and rho cutoff based on pseudo data

--- src/context/providers/PlanewaveCutoffsContextProvider.js
+++ src/context/providers/PlanewaveCutoffsContextProvider.js
@@ -40,20 +40,51 @@ export class PlanewaveCutoffsContextProvider extends mix(ContextProvider).with(
         return cutoffConfig[this.application.name];
     }
 
-    get defaultECUTWFC() {
-        let highestCutoff = 0;
+    getCutoffsFromPseudos = () => {
+        let ecutwfc = 0;
         (this.methodData.pseudo || []).forEach((element) => {
-            if (element.cutoffs.wfc.standard && element.cutoffs.wfc.standard > highestCutoff) {
-                highestCutoff = element.cutoffs.wfc.standard;
+            // set the highest of all elements
+            if (element.cutoffs.wfc.standard && element.cutoffs.wfc.standard > ecutwfc) {
+                ecutwfc = element.cutoffs.wfc.standard;
             }
         });
 
-        console.log(">>>>>>>", highestCutoff);
+        let ecutrho = 0;
+        (this.methodData.pseudo || []).forEach((element) => {
+            if (element.cutoffs.rho.standard && element.cutoffs.rho.standard > ecutrho) {
+                ecutrho = element.cutoffs.rho.standard;
+                // if rho cutoff is not present, set it based on wfc cutoff
+            } else if (this.methodData.pseudo.type === "us") {
+                // if it is ultrasoft pseudopotential set rho cutoff 8 times that of wfc cutoff
+                ecutrho = ecutwfc * 8;
+            } else {
+                // if it is not ultrasoft pseudopotential set rho cutoff 4 times that of wfc cutoff
+                ecutrho = ecutwfc * 4;
+            }
+        });
+
+        return [ecutwfc, ecutrho];
+    };
+
+    get defaultECUTWFC() {
+        const [ecutwfc] = this.getCutoffsFromPseudos();
+        console.log(">>>>>>>>", ecutwfc);
+
+        if (ecutwfc > 0) {
+            return ecutwfc;
+        }
 
         return this._cutoffConfigPerApplication.wavefunction || null;
     }
 
     get defaultECUTRHO() {
+        const [, ecutrho] = this.getCutoffsFromPseudos();
+        console.log("<<<<<<<<", ecutrho);
+
+        if (ecutrho > 0) {
+            return ecutrho;
+        }
+
         return this._cutoffConfigPerApplication.density || null;
     }
 
