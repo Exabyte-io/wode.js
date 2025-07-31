172f004a48af04ace3ef19d592ef450cfe813a06
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2025-07-31 22:18:13 +0800

    move get cutoffs from pseudo method to method data context mixin

--- src/context/mixins/MethodDataContextMixin.js
+++ src/context/mixins/MethodDataContextMixin.js
@@ -42,6 +42,50 @@ export function methodDataContextMixin(item) {
         get isMethodDataUpdated() {
             return Boolean(this.extraData && this.extraData.methodDataHash !== this.methodDataHash);
         },
+
+        /**
+         * @summary Find cutoff values corresponding to wavefunction or charge
+         * density with accuracy level in the pseudo method data
+         * @param data {Object}: the data object from pseudo method data
+         * @param cutoffEntity {String}: "wavefunction", or "density"
+         * @param accuracyLevel {String}: "standard", "high", or "low"
+         * @return {number}: if cutoff value present returns value else return 0
+         */
+        _cutoffFromPseudoData(data, cutoffEntity, accuracyLevel = "standard") {
+            const cutoff = data?.cutoffs?.[cutoffEntity] || [];
+            return cutoff.find((obj) => obj.accuracy_level === accuracyLevel)?.value ?? 0;
+        },
+
+        /**
+         * @summary Set highest cutoff of all elements present, in case wavefunction
+         * cutoff is present but no charge density cutoff, set charge density cutoff
+         * to 4 or 8 times that of wavefunction cutoff
+         * @return {Array<number>}: tuple of wavefunction and density cutoffs
+         */
+        get highestCutoffsFromPseudos() {
+            let ecutwfc = 0;
+            let ecutrho = 0;
+            const pseudos = this.methodData?.pseudo || [];
+
+            pseudos.forEach((data) => {
+                const ecutwfcStandard = this._cutoffFromPseudoData(data, "wavefunction");
+                const ecutrhoStandard = this._cutoffFromPseudoData(data, "density");
+                // set the highest cutoff of all elements
+                ecutwfc = Math.max(ecutwfc, ecutwfcStandard);
+
+                if (ecutrhoStandard > ecutrho) {
+                    ecutrho = ecutrhoStandard;
+                } else {
+                    // if rho cutoff is not present, set it based on wfc cutoff
+                    // if it is ultrasoft pseudopotential set rho cutoff 8 times
+                    // that of wfc cutoff, otherwise 4 times that of wfc cutoff
+                    const rhoMultiplier = data?.type === "us" ? 8 : 4;
+                    ecutrho = Math.max(ecutrho, ecutwfc * rhoMultiplier);
+                }
+            });
+
+            return [ecutwfc, ecutrho];
+        },
     };
 
     Object.defineProperties(item, Object.getOwnPropertyDescriptors(properties));
--- src/context/providers/PlanewaveCutoffsContextProvider.js
+++ src/context/providers/PlanewaveCutoffsContextProvider.js
@@ -38,52 +38,8 @@ export class PlanewaveCutoffsContextProvider extends ContextProvider {
         return cutoffConfig[this.application.name];
     }
 
-    /**
-     * @summary Find cutoff values corresponding to wavefunction or charge
-     * density with accuracy level in the pseudo method data
-     * @param data {Object}: the data object from pseudo method data
-     * @param cutoffEntity {String}: "wavefunction", or "density"
-     * @param accuracyLevel {String}: "standard", "high", or "low"
-     * @return {number}: if cutoff value present returns value else return 0
-     */
-    _cutoffFromPseudoData = (data, cutoffEntity, accuracyLevel = "standard") => {
-        const cutoff = data?.cutoffs?.[cutoffEntity] || [];
-        return cutoff.find((obj) => obj.accuracy_level === accuracyLevel)?.value ?? 0;
-    };
-
-    /**
-     * @summary Set highest cutoff of all elements present, in case wavefunction
-     * cutoff is present but no charge density cutoff, set charge density cutoff
-     * to 4 or 8 times that of wavefunction cutoff
-     * @return {Array<number>}: tuple of wavefunction and density cutoffs
-     */
-    get _cutoffsFromPseudos() {
-        let ecutwfc = 0;
-        let ecutrho = 0;
-        const pseudos = this.methodData?.pseudo || [];
-
-        pseudos.forEach((data) => {
-            const ecutwfcStandard = this._cutoffFromPseudoData(data, "wavefunction");
-            const ecutrhoStandard = this._cutoffFromPseudoData(data, "density");
-            // set the highest cutoff of all elements
-            ecutwfc = Math.max(ecutwfc, ecutwfcStandard);
-
-            if (ecutrhoStandard > ecutrho) {
-                ecutrho = ecutrhoStandard;
-            } else {
-                // if rho cutoff is not present, set it based on wfc cutoff
-                // if it is ultrasoft pseudopotential set rho cutoff 8 times
-                // that of wfc cutoff, otherwise 4 times that of wfc cutoff
-                const rhoMultiplier = data?.type === "us" ? 8 : 4;
-                ecutrho = Math.max(ecutrho, ecutwfc * rhoMultiplier);
-            }
-        });
-
-        return [ecutwfc, ecutrho];
-    }
-
     get defaultECUTWFC() {
-        const [ecutwfc] = this._cutoffsFromPseudos;
+        const [ecutwfc] = this.highestCutoffsFromPseudos;
 
         if (ecutwfc > 0) {
             return ecutwfc;
@@ -93,7 +49,7 @@ export class PlanewaveCutoffsContextProvider extends ContextProvider {
     }
 
     get defaultECUTRHO() {
-        const [, ecutrho] = this._cutoffsFromPseudos; // destructure and select second item
+        const [, ecutrho] = this.highestCutoffsFromPseudos; // destructure and select second item
 
         if (ecutrho > 0) {
             return ecutrho;
